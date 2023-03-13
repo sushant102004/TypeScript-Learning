1f1612a4adebf04665e5b99893e5d3680d8a9878
sushant102004 <sushant.dhiman9812@gmail.com>
2023-03-13 21:05:06 +0530

    "any" type

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let userName = 'Sushant';
+if (userName == 'Sushant') {
+    console.log('Welcome');
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+/*
+    Data types in TypeScript
+    1. any
+    2. unknown
+    3. never
+    4. enum
+    5. tuple
+    ++ all JS Types.
+*/
+
+// TS can automatically identify data type
+let userName = 'Sushant'
+let age = 19
+
+// Here we had not assined any data type and value so TS will use "any" datatype here.
+let none;
