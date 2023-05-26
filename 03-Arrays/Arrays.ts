4e4d7d6649b2bac50addd8c06f2efdaa163203ee
sushant102004 <sushant.dhiman9812@gmail.com>
2023-05-26 13:53:30 +0530

    Arrays

---
+++ 03-Arrays/Arrays.ts
@@ -0,0 +1,22 @@
+let names : string[] = ['Sushant', 'Kunal', 'John']
+
+let ages: Array<number> = [19, 19, 21]
+
+type Student = {
+    name : string
+    age : number
+    rollNo : number
+}
+
+let students: Student[] = [
+    {name : 'Sushant', age : 19, rollNo : 11212531},
+    {name : 'Kunal', age : 19, rollNo : 11212534}
+]
+
+// Multidimensional Array
+
+const cords : number[][] = [
+    [3, 2],
+    [1, 9],
+    [2, 6]
+]
