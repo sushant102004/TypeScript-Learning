b230156922f5a06dac7e0227003e908479ced7b1
sushant102004 <sushant.dhiman9812@gmail.com>
2023-05-26 11:55:11 +0530

    Object Based Return

--- 02-Objects/objects.ts
+++ 02-Objects/objects.ts
@@ -2,4 +2,12 @@ function printName(person: { first: string, last: string }) {
     console.log(`Hello ${person.first} ${person.last}`)
 }
 
-printName({first : 'Sushant', last: 'Dhiman'})
+printName({ first: 'Sushant', last: 'Dhiman' })
+
+
+let coordinate: { x: number, y: number } = { x: 0, y: 0 };
+
+function randomCoordinate(): { x: number, y: number } {
+    return { x: Math.random(), y: Math.random() }
+}
+console.log(randomCoordinate())
