8956da3f274ec6243901a69760dfef541c087716
sushant102004 <sushant.dhiman9812@gmail.com>
2023-05-27 14:31:58 +0530

    Interface Inheritance

--- 06-Interface/01-Interfaces.ts
+++ 06-Interface/01-Interfaces.ts
@@ -1,6 +1,12 @@
-interface StudentInterface {
+interface Person {
+    name: string,
+    age: number,
+    gender: string
+}
+
+
+interface StudentInterface extends Person {
     readonly id: number
-    name: string
     course: string
     isFeePaid: boolean
     phoneNumber: number
@@ -16,6 +22,8 @@ interface StudentInterface {
 const sushant: StudentInterface = {
     id: 11212531,
     name: 'Sushant Dhiman',
+    age: 19,
+    gender: 'Male',
     course: 'B.Tech CSE',
     isFeePaid: false,
     phoneNumber: 8813961989,
@@ -25,4 +33,5 @@ const sushant: StudentInterface = {
 }
 
 
+console.log(sushant)
 console.log(sushant.calculateScholarship(74))
