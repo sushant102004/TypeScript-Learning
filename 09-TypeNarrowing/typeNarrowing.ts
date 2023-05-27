3c45920557b142eca59bc684cc64f0db0326881c
sushant102004 <sushant.dhiman9812@gmail.com>
2023-05-27 23:18:54 +0530

    Basic Type Narrowing

---
+++ 09-TypeNarrowing/typeNarrowing.ts
@@ -0,0 +1,48 @@
+function triple(value: number | string) {
+    if (typeof value === 'number') {
+        return value * 3
+    } else if (typeof value === 'string') {
+        return value.repeat(3)
+    }
+}
+
+const printLetters = (word: string | null) => {
+    if (!word) console.log('Word is empty')
+
+    else for (let i = 0; i < word.length; i++) {
+        console.log(word[i])
+    }
+}
+
+interface Movie {
+    title: string,
+    duration: number
+}
+
+interface TVShow {
+    title: string,
+    numOfEpisodes: number,
+    episodeDuration: number
+}
+
+function getDuration(media: Movie | TVShow): number {
+    if ("numOfEpisodes" in media) {
+        return media.numOfEpisodes
+    } else {
+        return media.duration
+    }
+}
+
+const endGame: Movie = {
+    title: 'Avengers End Game',
+    duration: 2.5
+}
+
+const got: TVShow = {
+    title: 'Game Of Thrones',
+    episodeDuration: 50,
+    numOfEpisodes: 80
+}
+
+console.log(getDuration(endGame))
+console.log(getDuration(got))
